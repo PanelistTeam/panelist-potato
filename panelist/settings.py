2388078d9645bf8504971673b7cbb5b0990ac397
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-04-26 01:50:14 +0200

    Fix PEP8 warnings

--- panelist/settings.py
+++ panelist/settings.py
@@ -22,7 +22,6 @@ except ImportError:
     from .security_settings import *
 
 
-
 ALLOWED_HOSTS = []
 
 
@@ -52,8 +51,7 @@ ROOT_URLCONF = 'panelist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

2ccf49b7f7ca699c33310b302ce350169ea65501
Bartosz Rodziewicz <bartoszka1996@gmail.com>
2018-04-26 00:25:31 +0200

    Split Django settings file for letting us have secure config out of repo

--- panelist/settings.py
+++ panelist/settings.py
@@ -11,19 +11,15 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from shutil import copyfile
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+try:
+    from .secure_settings import *
+except ImportError:
+    copyfile('panelist/secure_settings.py.default', 'panelist/secure_settings.py')
+    from .secure_settings import *
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@-wt(u2ugz4tq#5s)2!@vwi7%l%)68v&_u*hm31swa^03(xcdj'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -69,17 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'panelist.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
